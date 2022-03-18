3d1d593fc85d6e0c88c746990cf5a05e2b70e6a8
Emile <emile.bettini@protonmail.ch>
2022-03-18 15:25:32 +0100

    Update managers.py

--- hardware/managers.py
+++ hardware/managers.py
@@ -1,5 +1,15 @@
+from decimal import Decimal
+from typing import Dict
+
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Sum
 
 
 class HardwareManager(models.Manager):
-    ...
+
+    def listing_by_model(self, model: str, user: User) -> Dict[str, Decimal | int]:
+        return self.filter(model=model, article__user=user).aggregate(
+            quantity=Sum('article__quantity', default=0),
+            price=Sum('article__price', default=0)
+        )
