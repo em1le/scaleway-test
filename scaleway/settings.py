aabfd6f0cd8c2a54044847b9fcd2ae1698ad7aa3
Emile <emile.bettini@protonmail.ch>
2022-03-18 15:34:12 +0100

    Update settings.py

--- scaleway/settings.py
+++ scaleway/settings.py
@@ -127,3 +127,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/dashboard"
+LOGIN_URL = "/accounts/login/"
+LOGOUT_REDIRECT_URL = "/"
