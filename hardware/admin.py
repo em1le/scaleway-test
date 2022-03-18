72e1428e6825b764544443e448b426c147bfdf3c
Emile <emile.bettini@protonmail.ch>
2022-03-18 14:34:23 +0100

    add admins

--- hardware/admin.py
+++ hardware/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from hardware.models import Hardware
+
+
+@admin.register(Hardware)
+class HardwareAdmin(admin.ModelAdmin):
+    list_display = ('brand', 'model', 'serial_number')
