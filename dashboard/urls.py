3baaa90468d6cae9fc18499d38ff4f7e3bba1999
Emile <emile.bettini@protonmail.ch>
2022-03-18 15:00:16 +0100

    add dashboad url and view, update success signup

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,4 +1,9 @@
+from django.contrib.auth.decorators import login_required
+from django.urls import path
+
+from dashboard.views import DashboardView
 
 
-
-urlpatterns = []
+urlpatterns = [
+    path("", login_required(DashboardView.as_view()), name="list-article")
+]
---
+++ dashboard/views.py
@@ -0,0 +1,21 @@
+from typing import Dict, Any
+
+from django.db.models import QuerySet
+from django.views.generic import ListView
+
+from article.models import Article
+
+
+class DashboardView(ListView):
+    model = Article
+    template_name = "dashboard/dashboard.html"
+    context_object_name = "articles"
+    paginate_by = 5
+
+    def get_queryset(self) -> QuerySet:
+        return self.request.user.article_set.order_by('-created')
+
+    def get_context_data(self, **kwargs) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        return context
+    
