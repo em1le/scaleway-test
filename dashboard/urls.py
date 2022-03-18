3725cb8d77ca5af5813305451d069d8bd4472c21
Emile <emile.bettini@protonmail.ch>
2022-03-18 15:09:11 +0100

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 
-from dashboard.views import DashboardView, CreateHardwareView
+from dashboard.views import DashboardView, CreateHardwareView, CreateArticleView
 
 urlpatterns = [
     path("", login_required(DashboardView.as_view()), name="list-article"),
