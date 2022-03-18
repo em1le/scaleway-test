20a60ba81ec8675b29540d6dcf99111095477238
Emile <emile.bettini@protonmail.ch>
2022-03-18 15:24:12 +0100

    add factories

--- hardware/tests.py
+++ hardware/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from hardware.models import Hardware
+from article.factories import ArticleFactory
+
+
+class HardwareTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.article = ArticleFactory()
+        super().setUpTestData()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        super().tearDownClass()
+
+    def test_listing_by_model(self):
+        expected = {"quantity": self.article.quantity, "price": self.article.price}
+        hardware_model = self.article.hardware.model
+        result = Hardware.objects.listing_by_model(hardware_model, self.article.user)
+        self.assertDictEqual(result, expected)
