1e1a5f5228853e2558d9f55f29109eb78980d1b9
Emile <emile.bettini@protonmail.ch>
2022-03-18 15:41:34 +0100

    add rest_framework to installed app and create srz and viewsets

---
+++ hardware/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from hardware.models import Hardware
+
+
+class HardwareSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Hardware
+        fields = "__all__"
---
+++ hardware/viewsets.py
@@ -0,0 +1,12 @@
+from django.db.models import QuerySet
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from hardware.models import Hardware
+from hardware.serializers import HardwareSerializer
+
+
+class EntityViewSet(viewsets.ModelViewSet):
+    serializer_class = HardwareSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Hardware.objects.all()
